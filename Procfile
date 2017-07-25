8053e04e8512c3a79c32a452514a7761a90ffb62
Hao Wu <haowu0802@gmail.com>
2017-07-24 20:27:12 -0700

    add Sprint model
    + Sprint model
    + migration 0001

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn board
+web: gunicorn scrum.wsgi
--- board/models.py
+++ board/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Sprint(models.Model):
+    """Development iteration period."""
+
+    name = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField(blank=True, default='')
+    end = models.DateField(unique=True)  # only one spring can be ended at one moment of time
+
+    def __str__(self):
+        return self.name or _('Sprint ending %s') % self.end  # to string
