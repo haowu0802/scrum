507c5f23d1f02de5b7f23b4b4fb99c0adeceac4c
Hao Wu <haowu0802@gmail.com>
2017-07-24 17:13:00 -0700

    more staticfile setting for WhiteNoise
    more staticfile setting for WhiteNoise

--- scrum/settings.py
+++ scrum/settings.py
@@ -121,5 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles'),  # parse root for static res
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),  # parse root for static res
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
