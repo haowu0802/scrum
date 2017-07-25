5fd913c72234344e181cbc15ad3f885cd8107ed7
Hao Wu <haowu0802@gmail.com>
2017-07-25 12:26:18 -0700

    add links field to resource self

--- board/serializers.py
+++ board/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers  # serializers lib from django-rest
+from rest_framework.reverse import reverse  # for producing links for resource
 
 from .models import Sprint, Task  # the board models
 from django.contrib.auth import get_user_model  # to get a clean user model
@@ -7,37 +8,70 @@ User = get_user_model()  # a clean user model for Task foreign key
 
 
 class SprintSerializer(serializers.ModelSerializer):
+
+    links = serializers.SerializerMethodField()  # links to related resource
+
     class Meta:
         model = Sprint
-        fields = ('id', 'name', 'description', 'end', )
+        fields = ('id', 'name', 'description', 'end', 'links', )
+
+    def get_links(self, obj):
+        """produce links to related resource"""
+        request = self.context['request']
+        return {
+            'self': reverse('sprint-detail',  # link to detail page of itself
+                            kwargs={'pk': obj.pk},
+                            request=request),
+        }
 
 
 class TaskSerializer(serializers.ModelSerializer):
 
     # get text for status code, to show text instead of code
     status_display = serializers.SerializerMethodField()
-
     assigned = serializers.SlugRelatedField(  # replace assigned with user name instead of user key id
         slug_field=User.USERNAME_FIELD,
         required=False,
         read_only=True
     )
+    links = serializers.SerializerMethodField()  # links to related resource
 
     class Meta:
         model = Task
         fields = ('id', 'name', 'description', 'sprint',
                   'status', 'status_display', 'order',
-                  'assigned', 'started', 'due', 'completed', )
+                  'assigned', 'started', 'due', 'completed', 'links', )
 
     def get_status_display(self, obj):
         return obj.get_status_display()
+
+    def get_links(self, obj):
+        """produce links to related resource"""
+        request = self.context['request']
+        return {
+            'self': reverse('task-detail',  # link to detail page of itself
+                            kwargs={'pk': obj.pk},
+                            request=request),
+        }
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     # function from user model, an interface for user model and custom user inherited auth.models
     full_name = serializers.CharField(source='get_full_name', read_only=True)
+    links = serializers.SerializerMethodField()  # links to related resource
 
     class Meta:
         model = User
-        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
+        fields = ('id', User.USERNAME_FIELD, 'full_name',
+                  'is_active', 'links', )
+
+    def get_links(self, obj):
+        """produce links to related resource"""
+        request = self.context['request']
+        username = obj.get_username()
+        return {
+            'self': reverse('user-detail',  # link to detail page of itself
+                            kwargs={User.USERNAME_FIELD: username},
+                            request=request),
+        }
