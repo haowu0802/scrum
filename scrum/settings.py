f522aa0146af7b7b7171633e444055bf182a5d8c
Hao Wu <haowu0802@gmail.com>
2017-07-24 21:18:17 -0700

    add dj_database_url
    for reading database config from env var

--- scrum/settings.py
+++ scrum/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url  # for connecting to Heroku DB
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +86,9 @@ DATABASES = {
         'NAME': 'scrum',  # db name scrum
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)  # get db from env vars
+DATABASES['default'].update(db_from_env)  # update DB
 
 
 # Password validation
